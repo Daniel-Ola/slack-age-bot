82f42d54fa268e09df441ad2485a38e4a6b5b888
Daniel Olanrewaju <danorelanre@gmail.com>
2022-08-31 20:48:25 +0100

    setting env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
 	"log"
 	"os"
@@ -20,11 +21,23 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	}
 }
 
-func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4010050576038-4010120797846-25hEc7SQzeG69op8arWRDRNC")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A040V8Q8GBB-4010072985622-857c741643ce25f9c8374ddb030d0c1f8cef544d17055af8967a33c7030f30b1")
+func goGetEnvVar(key string) string {
+	// load .env file
+	err := godotenv.Load(".env")
 
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+
+	/*botToken := goGetEnvVar("SLACK_BOT_TOKEN")
+	appToken := goGetEnvVar("SLACK_APP_TOKEN")*/
+
+	bot := slacker.NewClient("xoxb-4010050576038-4010120797846-25hEc7SQzeG69op8arWRDRNC", "xapp-1-A040V8Q8GBB-4010072985622-857c741643ce25f9c8374ddb030d0c1f8cef544d17055af8967a33c7030f30b1")
 
 	go printCommandEvents(bot.CommandEvents())
 
